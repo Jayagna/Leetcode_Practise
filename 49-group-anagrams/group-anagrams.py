9750800b8514b786532d4868a0a06f2330b67599
Jayagna <jayagnapatel15@gmail.com>
2026-01-10 12:18:35 +0530

    Time: 35 ms (21.99%) | Memory: 18.6 MB (7.62%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,17 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
         res = defaultdict(list)
 
         for s in strs:
             count = [0]*26
-            for c in s:
+
+            for c in s :
                 count[ord(c) - ord("a")] += 1
+
             res[tuple(count)].append(s)
-        
         return list(res.values())
+        
