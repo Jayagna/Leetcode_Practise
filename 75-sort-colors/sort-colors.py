a4e7a81d3a6430edaebc7a8d02922e4ef17df0c0
Jayagna <jayagnapatel15@gmail.com>
2026-02-03 14:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.00%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,5 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        l = 0
+        for j in range(2):
+            for i in range(len(nums)):
+                if (nums[i] == j):
+                    nums[i],nums[l] = nums[l],nums[i]
+                    l += 1
+        return nums
         
