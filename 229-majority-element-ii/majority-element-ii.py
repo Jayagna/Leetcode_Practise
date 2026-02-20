58e1be064bddfcd50107fe6509d75fb3d308a2ef
Jayagna <jayagnapatel15@gmail.com>
2026-02-20 20:28:15 +0530

    Time: 19 ms (21.88%) | Memory: 14.1 MB (67.80%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -4,15 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        times = int(len(nums)/3)
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
+        count  = defaultdict(int)
+
+        for n in nums:
+            count[n] += 1
+
+            if len(count) <= 2:
+                continue
+            
+            new_count  = defaultdict(int)
+            for n,c in count.items():
+                if c>1:
+                    new_count[n] = c-1
+            count = new_count    
 
         res = []
-        for k,v in count.items() : 
-            if v > times:
-                res.append(k)
+        for n in count:
+            if nums.count(n) > len(nums)//3:
+                res.append(n)
+        return res
 
         return res    
         
