234edb5ee32118f7cb390adef78a290fca071e58
Jayagna <jayagnapatel15@gmail.com>
2026-03-31 21:40:44 +0530

    Time: 171 ms (45.78%) | Memory: 26.8 MB (75.47%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: List[int]
         """
         res = [0] * len(temperatures)
-        stack = []  # pair: [temp, index]
+        stack = []
 
         for i, t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
