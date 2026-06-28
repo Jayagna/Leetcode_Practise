c5fc5bf3b1d0c496186ba2e261b7fef2e7000a6e
Jayagna <jayagnapatel15@gmail.com>
2026-06-28 21:31:27 +0530

    Time: 49 ms (27.06%) | Memory: 20.8 MB (62.02%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,10 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        hset = set()
-        for num in nums:
-            if num not in hset:
-                hset.add(num)
-            else:
-                return num
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
         
