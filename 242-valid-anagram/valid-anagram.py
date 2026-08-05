cb9d83a2e9cd4663b1992feed17a15583f877bd4
Jayagna <jayagnapatel15@gmail.com>
2026-08-05 18:17:01 +0530

    Time: 3 ms (98.43%) | Memory: 19.5 MB (46.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if (len(s) != len(t)) :
-            return False
-        countT, countS = {} , {}
-        for letter in s :
-            countS[letter] = 1 + countS.get(letter,0)
-        for letter in t : 
-            countT[letter] = 1 + countT.get(letter,0)
-
-        return countS == countT
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
